bdb8a34a55ea9443cac615b609f399a42611233e
grudev888 <happydream9028@gmail.com>
2024-01-23 07:06:35 +0900

    fix: remove mock api

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,6 @@ import { CacheProvider } from '@emotion/react'
 import { selectCurrentLanguageDirection } from 'app/store/i18nSlice'
 import themeLayouts from 'app/theme-layouts/themeLayouts'
 import { selectMainTheme } from '@fuse/core/FuseSettings/store/fuseSettingsSlice'
-import MockAdapterProvider from '@mock-api/MockAdapterProvider'
 import axios from 'axios'
 import withAppProviders from './withAppProviders'
 import { AuthRouteProvider } from './auth/AuthRouteProvider'
@@ -46,29 +45,27 @@ function App() {
   const mainTheme = useSelector(selectMainTheme)
 
   return (
-    <MockAdapterProvider>
-      <CacheProvider
-        value={createCache(emotionCacheOptions[langDirection] as Options)}
-      >
-        <FuseTheme theme={mainTheme} direction={langDirection}>
-          <AuthRouteProvider>
-            <SnackbarProvider
-              maxSnack={5}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
-              }}
-              classes={{
-                containerRoot:
-                  'bottom-0 right-0 mb-52 md:mb-68 mr-8 lg:mr-80 z-99',
-              }}
-            >
-              <FuseLayout layouts={themeLayouts} />
-            </SnackbarProvider>
-          </AuthRouteProvider>
-        </FuseTheme>
-      </CacheProvider>
-    </MockAdapterProvider>
+    <CacheProvider
+      value={createCache(emotionCacheOptions[langDirection] as Options)}
+    >
+      <FuseTheme theme={mainTheme} direction={langDirection}>
+        <AuthRouteProvider>
+          <SnackbarProvider
+            maxSnack={5}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            classes={{
+              containerRoot:
+                'bottom-0 right-0 mb-52 md:mb-68 mr-8 lg:mr-80 z-99',
+            }}
+          >
+            <FuseLayout layouts={themeLayouts} />
+          </SnackbarProvider>
+        </AuthRouteProvider>
+      </FuseTheme>
+    </CacheProvider>
   )
 }
 
